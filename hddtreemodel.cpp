989cb0a802d8a745e49984a389fcddff8c1f8ac4
maodlife <maodlife@icloud.com>
2024-10-30 22:44:45 +0800

    tree view color

--- hddtreemodel.cpp
+++ hddtreemodel.cpp
@@ -1,17 +1,29 @@
 #include "hddtreemodel.h"
+#include <QBrush>
 
 HddTreeModel::HddTreeModel(std::shared_ptr<HddTreeNode> ptr) : TreeModel(ptr) {}
 
-QVariant HddTreeModel::data(const QModelIndex &index, int role) const
-{
-    if (!index.isValid() || role != Qt::DisplayRole) {
+QVariant HddTreeModel::data(const QModelIndex &index, int role) const {
+    if (!index.isValid()) {
         return QVariant();
     }
     HddTreeNode *ptr = static_cast<HddTreeNode *>(index.internalPointer());
-    if (ptr->saveData.path.isEmpty())
-        return ptr->name;
-    else
-        return "*" + ptr->name;
+
+    if (role == Qt::DisplayRole) {
+        if (ptr->saveData.path.isEmpty())
+            return ptr->name;
+        else
+            return "*" + ptr->name;
+    }
+
+    if (role == Qt::ForegroundRole) {
+        if (ptr->saveData.path.isEmpty())
+            return QBrush(Qt::black);
+        else
+            return QBrush(Qt::red);
+    }
+
+    return QVariant();
 }
 
 void HddTreeModel::Declare(const QModelIndex &index, QString path)
